185a4da35fc15eab0c27e0a0187c13cee276cd43
Brooks_Wilcox <brookswill88@gmail.com>
2022-01-12 18:21:04 -0700

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,81 @@
 # your code goes here
+
+class Person
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(amount)
+        if amount > 10
+            @happiness = 10
+        elsif amount < 0
+            @happiness = 0
+        else
+            @happiness = amount
+        end
+    end
+
+    def hygiene=(amount)
+        if amount > 10
+            @hygiene = 10
+        elsif amount < 0
+            @hygiene = 0
+        else
+            @hygiene = amount
+        end
+    end
+
+    def happy?
+        @happiness > 7
+    end
+
+    def clean?
+        @hygiene > 7
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene = hygiene + 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness = happiness + 2
+        self.hygiene = hygiene - 3
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness = happiness + 3
+        friend.happiness = friend.happiness + 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness = happiness - 2
+            friend.happiness = friend.happiness - 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness = happiness + 1
+            friend.happiness = friend.happiness + 1
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
+    end
+
+end
+
+
+
